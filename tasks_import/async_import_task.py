9787f796e148747ff3120f15f72f02bfb9b6acba
sayhaya <sayhaya@qq.com>
2019-06-03 15:25:28 +0800

    add import dajie task

---
+++ tasks_import/async_import_task.py
@@ -0,0 +1,68 @@
+import asyncio
+import json
+import time
+
+import redis
+import aredis
+
+redis_pool = aredis.ConnectionPool(
+    host='10.0.0.48',
+    port=6379,
+    db=1
+)
+
+
+# def redis_cli():
+#     return redis.StrictRedis(
+#         connection_pool=redis_pool,
+#         decode_responses=True,  # 自动解码
+#     )
+
+
+def aredis_cli():
+    return aredis.StrictRedis(
+        connection_pool=redis_pool,
+        decode_responses=True,  # 自动解码
+    )
+
+
+TYPE1_NUM = 3
+
+
+class TaskModel():
+    def __init__(self, *args, **kwargs):
+        self.redis_client = aredis_cli()
+
+    async def push(self, semaphore, site, type, value):
+        async with semaphore:
+            await self.redis_client.lpush('{}_{}'.format(site, type), value)
+
+
+if __name__ == '__main__':
+
+    file = 'key_dajie.list'
+
+
+    def company():
+        with open(file, 'r', encoding='utf-8') as f:
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                yield line.strip()
+
+
+    def gene(to_fill):
+        task = {'page': 1, 'site': 'dajie', 'type': 1, 'keyword': to_fill}
+        return json.dumps(task, ensure_ascii=False)
+
+
+    loop = asyncio.get_event_loop()
+    t = TaskModel().push
+    semaphore = asyncio.Semaphore(500)
+    task = [asyncio.ensure_future(t(semaphore, site ,type=1, value=gene(i))) for i in company()]
+    start = time.time()
+    loop.run_until_complete(asyncio.wait(task))
+    endtime = time.time() - start
+    print(endtime)
+    loop.close()
