057ecfbe53d802da4be1cfe421c92a7df1102e5f
sayhaya <sayhaya@qq.com>
2019-06-05 13:18:58 +0800

    添加 mongo 计时

--- crawler/extractor/mongo_operate.py
+++ crawler/extractor/mongo_operate.py
@@ -41,7 +41,7 @@ def company_update_func(resume):
         MT_cp.update({"jx_resume_id": resume["jx_resume_id"]}, resume)
         cards.incr(f'{resume["source"]}_{time.strftime("%Y-%m-%d", time.localtime())}')
         logging.info(f"updated jx_resume_id: {resume['jx_resume_id']}")
-    logging.info(f"jx_resume_id: {resume['jx_resume_id']} mongo operate cost {(time.time() - st) * 1000:.3f} ms.")
+    logging.info(f"jx_resume_id: {resume['jx_resume_id']} mongo operate cost {(time.time() - st):.3f} s.")
 
 
 def mongo_ur(resume: dict):
