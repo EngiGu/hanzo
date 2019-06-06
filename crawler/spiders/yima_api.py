6dd908ccedf58c9625bf06e3c7e3fdc9d2689778
sayhaya <sayhaya@qq.com>
2019-06-06 12:11:05 +0800

    hr58

--- crawler/spiders/yima_api.py
+++ crawler/spiders/yima_api.py
@@ -123,7 +123,7 @@ class Yima():
             logging.info("get message:{}".format(text))
             try:
                 print('yanzhengmaz;',text)
-                m = re.findall(r'\d{4,}', text)[0]
+                m = re.findall(r'\d{6}', text)[0]
             except:
                 logging.info("extract message failed, retry...")
                 continue
