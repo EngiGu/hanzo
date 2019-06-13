b0e889ef55b06926a9b095f086cfb5d784c6d71c
weiyang-1 <34814137+weiyang-1@users.noreply.github.com>
2019-06-13 14:50:20 +0800

    add juzi accounts

--- crawler/account.py
+++ crawler/account.py
@@ -7,7 +7,14 @@ IT_Juzi_Accounts = {
         {"user_id": "103", "user_name": "13104090498", "password": "pass"},
         {"user_id": "104", "user_name": "13104593209", "password": "pass"},
         {"user_id": "105", "user_name": "13316487995", "password": "pass"},
-        {"user_id": "106", "user_name": "15243271043", "password": "pass"}
+        {"user_id": "106", "user_name": "15154319942", "password": "pass"},
+        {"user_id": "107", "user_name": "13596335293", "password": "pass"},
+        {"user_id": "108", "user_name": "13664420744", "password": "pass"},
+        {"user_id": "109", "user_name": "13943241763", "password": "pass"},
+        {"user_id": "110", "user_name": "13943258254", "password": "pass"},
+        {"user_id": "111", "user_name": "13944290103", "password": "pass"},
+        {"user_id": "112", "user_name": "13944296934", "password": "pass"},
+        {"user_id": "113", "user_name": "15243271043", "password": "pass"}
     ]
 }
 
