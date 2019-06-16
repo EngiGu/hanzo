ea4e58579899dea6bec0a3e4110e72102cc0d677
sayheya <sayheya@163.com>
2019-06-16 09:43:43 +0800

    fix

--- tasks_import/async_import_task_hr58.py
+++ tasks_import/async_import_task_hr58.py
@@ -29,8 +29,10 @@ class TaskModel():
 
 
 if __name__ == '__main__':
+
     # file = 'hr58.txt'
-    site = 'hr58n'
+    site = 'hr58'
+
 
     # def company():
     #     with open(file, 'r', encoding='utf-8') as f:
@@ -40,22 +42,8 @@ if __name__ == '__main__':
     #                 break
     #             tmp = line.strip().split(' ')
     #             yield tmp[0] + '+' + tmp[2]
-    area = ['hongshan',
-            'wuchang',
-            'jiangan',
-            'jiangxia',
-            'hanyang',
-            'jianghan',
-            'qiaokou',
-            'dongxihu',
-            'huangpo',
-            'whtkfq',
-            'whqingshanqu',
-            'caidian',
-            'xinzhouqu',
-            'hannan',
-            'wuhan']
-
+    area = [159, 160, 161, 162, 163, 165, 166, 167, 168, 169, 170, 171, 1913, 8000, 15298]
+    area = [159]
 
     def gene(to_fill):
         task = {'page': 1, 'site': site, 'type': 1, 'keyword': to_fill}
@@ -65,7 +53,7 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     t = TaskModel().push
     semaphore = asyncio.Semaphore(500)
-    task = [asyncio.ensure_future(t(semaphore, site, type=1, value=gene(str(i)))) for i in area]
+    task = [asyncio.ensure_future(t(semaphore, site, type=1, value=gene(str(i) + '+0'))) for i in area]
     start = time.time()
     loop.run_until_complete(asyncio.wait(task))
     endtime = time.time() - start
