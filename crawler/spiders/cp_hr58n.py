b45606c06f3e05c19380e9fd4a0cfcdd0ba12a9c
sayhaya <sayhaya@qq.com>
2019-06-21 10:09:17 +0800

    add 58fru

--- crawler/spiders/cp_hr58n.py
+++ crawler/spiders/cp_hr58n.py
@@ -1,7 +1,7 @@
 import base64
 import hashlib
 import io
-import json,sys
+import json, sys
 import os, time
 import random
 from selenium.webdriver import Chrome
@@ -81,7 +81,6 @@ class DaJie(SpiderBase, Base):
         self.fr_times = 0
         # self.login()
 
-
     def check_is_login(self):
         url = 'https://employer.58.com/resumesearch'
         kwargs = {
@@ -92,51 +91,117 @@ class DaJie(SpiderBase, Base):
             return False
         return True
 
+    #
+    #
+    # def login(self):
+    #     l =  self.l
+    #     self.call_login_times += 1
+    #     if self.call_login_times > 10:
+    #         l.info(f"login be called: {self.call_login_times}, exit...")
+    #         sys.exit()
+    #
+    #     cookies_path = os.path.join(ROOT_PATH, 'cookies')
+    #     if not os.path.exists(cookies_path):
+    #         os.makedirs(cookies_path)
+    #
+    #     cookies_name =  f'cookies_{self.uid}'
+    #     # if not os.path.exists(cookies_name):
+    #
+    #     if self.first:
+    #         if os.path.exists(cookies_name):
+    #             self.first = False
+    #             with open(os.path.join(cookies_path, cookies_name), 'r') as f:
+    #                 cookies = f.read().strip()
+    #                 l.info(f'loaded local cookies: {cookies}')
+    #                 self.s.headers['cookie'] = cookies
+    #             if self.check_is_login():
+    #                 l.info(f'local cookies useful, login success...')
+    #                 return True
+    #
+    #     # login with selenium
+    #     for _ in range(10):
+    #         l.info(f"no cookies, starting {_+1}/10 login...")
+    #         cookie_str =  self._login()
+    #         if cookie_str:
+    #             self.s.headers['cookie'] = cookie_str
+    #             # self.cookies = cookies
+    #             # self.s.cookies = requests.utils.cookiejar_from_dict(cookies)
+    #             with open(os.path.join(cookies_path, cookies_name), 'w') as f:
+    #                 f.write(str(cookie_str))
+    #             return
+    #         try:
+    #             self.driver.quit()
+    #         except:
+    #             pass
+    #     l.error(f"after 10 retry, failed to login, exit....")
+    #     sys.exit()
 
+    def check_is_exists(self, phone):
+        l = self.l
+        headers = {
+            'authority': 'passport.58.com',
+            'pragma': 'no-cache',
+            'cache-control': 'no-cache',
+            'origin': 'https://passport.58.com',
+            'upgrade-insecure-requests': '1',
+            'content-type': 'application/x-www-form-urlencoded',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'referer': 'https://passport.58.com/login/?path=https%3A//wh.58.com/&PGTID=0d100000-0009-e96c-3106-f98464aa467e&ClickID=2',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'zh-CN,zh;q=0.9,it;q=0.8',
+            # 'cookie': 'id58=c5/njVzAJMoIAfoXA3VlAg==; 58tj_uuid=329c9551-6862-43d4-9fbe-4cd1ed969770; als=0; xxzl_deviceid=cp3Mvh7aF0HWvoYmymkjF8hlRJrKFfKVoLB%2BJeerMqgmiahmSXbkf35ND%2BYmMc9Z; wmda_uuid=57aa741ccc8947f8344a4d1bd77f9113; wmda_new_uuid=1; gr_user_id=5d8d983a-96d9-431d-b81f-185f802c5695; mcity=wh; mcityName=%E6%AD%A6%E6%B1%89; nearCity=%5B%7B%22cityName%22%3A%22%E6%AD%A6%E6%B1%89%22%2C%22city%22%3A%22wh%22%7D%5D; Hm_lvt_5a7a7bfd6e7dfd9438b9023d5a6a4a96=1556098240; cookieuid1=mgjwFVzALL4ODHp9A1s4Ag==; wmda_visited_projects=%3B1731916484865%3B6333604277682%3B2286118353409%3B3381039819650%3B4200524323842%3B4785068453378%3B7790950805815; showOrder=1; 58home=wh; city=wh; __utma=253535702.405960827.1559098468.1559098468.1559098468.1; __utmz=253535702.1559098468.1.1.utmcsr=wh.58.com|utmccn=(referral)|utmcmd=referral|utmcct=/job/; hots=%5B%7B%22d%22%3A0%2C%22s1%22%3A%22%E6%AD%A6%E6%B1%89%E4%BA%BA%E7%91%9E%E4%BA%BA%E5%8A%9B%E8%B5%84%E6%BA%90%E6%9C%8D%E5%8A%A1%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8%EF%BC%88%E9%A6%99%E8%8D%89%E6%8B%9B%E8%81%98%EF%BC%89%22%2C%22s2%22%3A%22%22%2C%22n%22%3A%22sou%22%7D%5D; xxzl_smartid=3ea03942f455c5e049f95aea1abd1d85; param8616=1; param8716kop=1; isSmartSortTipShowed=true; show_zcm_banner=true; sessionid=33b39c53-0fe8-499a-8259-159dd0fe2618; resumeOrder=time; finger_session=OIGeShH2-mRwPOOJEreO-bH-rrdUOuyl; lastuname=13148304735; vip=vipusertype%3D11%26vipuserpline%3D0%26v%3D1%26vipkey%3Df7d0d4d3b7564abad39591acf41cb2cc%26masteruserid%3D59352683440914; crmvip=""; dk_cookie=""; new_uv=69; utm_source=; spm=; init_refer=https%253A%252F%252Fgraph.qq.com%252Foauth2.0%252Fshow%253Fwhich%253DLogin%2526display%253Dpc%2526autoLogin%253D0%2526response_type%253Dcode%2526client_id%253D200065%2526scope%253Dget_user_info%252Cget_qq_level%252Cget_info%252Clist_album%252Cget_fanslist%2526state%253DGUl4_H0pCUn7_jxJ5tnfN-alrxS0jJzm%2526redirect_uri%253Dhttps%25253A%25252F%25252Fpassport.58.com%25252Fthd%25252Foauthlogin%25252Fpc%25252Fqzone%25253Fpath%25253Dhttps%2525253A%2525252F%2525252Fwh.58.com%2525252F%2525253Fpts%2525253D1561010802256%252526source%25253D58-default-pc; ppStore_fingerprint=860F18A32617495B64C24011EAA3E9CC3C5F76C9BF0DD291%EF%BC%BF1561011415484; new_session=0',
+        }
+
+        data = {
+            # 'fingerprint': 'OIGeShH2-mRwPOOJEreO-bH-rrdUOuyl',
+            'callback': 'successFun',
+            'username': str(phone),
+            'password': '6eef7d44b9a374bfde2309b96d92a8303b41671954cacf10710df52b9e7f1e19c8b843466dbfceb14244000fd46558671c93d79cbc4ee1a8435720d4104db74c772c670e8a31f300dba21475f4a5fda7e3d729102523c060c60689f2ba2690d01fcd84d478eeeb315143215f6e3a9c0c42626f1ea8f41d3db0a92714eaf19362',
+            # 'token': 'GlYcinGHn_kudlcr-9g-wXoYX5l2B-Hn',
+            'source': '58-default-pc',
+            'path': 'https%3A%2F%2Fwh.58.com%2F%3Fpts%3D1561011755558',
+            'domain': '58.com',
+            # 'finger2': 'zh-CN|24|1|8|1920_1080|1920_1040|-480|1|1|1|undefined|1|unknown|Win32|unknown|3|true|false|false|false|false|0_false_false|d41d8cd98f00b204e9800998ecf8427e|dbaff747fffe0586df0c750042298498',
+            'psdk-d': 'jsdk',
+            'psdk-v': '1.0.0'
+        }
+        response = requests.post('https://passport.58.com/58/login/pc/dologin', headers=headers, data=data)
+        l.info(f'check res: {response.content.decode()}')
+        if '该用户不存在' in response.content.decode():
+            return False
+        return True
+
+    def read_or_save_cookies(self, read=True, cookies=None):
+        p = './hr58n.cookies'
+        if read:
+            if not os.path.exists(p):
+                return None
+            with open(p, 'r', encoding='utf-8') as f:
+                return f.read().strip()
+        else:
+            with open(p, 'w', encoding='utf-8') as f:
+                return f.write(cookies)
 
     def login(self):
-        l =  self.l
-        self.call_login_times += 1
-        if self.call_login_times > 10:
-            l.info(f"login be called: {self.call_login_times}, exit...")
-            sys.exit()
+        l = self.l
 
-        cookies_path = os.path.join(ROOT_PATH, 'cookies')
-        if not os.path.exists(cookies_path):
-            os.makedirs(cookies_path)
-
-        cookies_name =  f'cookies_{self.uid}'
-        # if not os.path.exists(cookies_name):
-
-        if self.first:
-            if os.path.exists(cookies_name):
-                self.first = False
-                with open(os.path.join(cookies_path, cookies_name), 'r') as f:
-                    cookies = f.read().strip()
-                    l.info(f'loaded local cookies: {cookies}')
-                    self.s.headers['cookie'] = cookies
-                if self.check_is_login():
-                    l.info(f'local cookies useful, login success...')
-                    return True
-
-        # login with selenium
-        for _ in range(10):
-            l.info(f"no cookies, starting {_+1}/10 login...")
-            cookie_str =  self._login()
-            if cookie_str:
-                self.s.headers['cookie'] = cookie_str
-                # self.cookies = cookies
-                # self.s.cookies = requests.utils.cookiejar_from_dict(cookies)
-                with open(os.path.join(cookies_path, cookies_name), 'w') as f:
-                    f.write(str(cookie_str))
+        save_cookies  = self.read_or_save_cookies(read=True)
+        if save_cookies:
+            self.s.headers['cookie'] = save_cookies
+            if self.check_is_login():
+                l.info(f'using save cookies...')
                 return
-            try:
-                self.driver.quit()
-            except:
-                pass
-        l.error(f"after 10 retry, failed to login, exit....")
-        sys.exit()
 
+        for _ in range(10):
+            try:
+                cookie_str = self._login()
+                self.s.headers['cookie'] = cookie_str
+                l.info(f"login success...")
+                self.read_or_save_cookies(read=False, cookies=cookie_str)
+                break
+            except Exception as e:
+                l.info(f"login error: {str(e)}")
 
     def _login(self):
         l = self.l
@@ -152,10 +217,21 @@ class DaJie(SpiderBase, Base):
         driver.find_elements_by_xpath('//*[@id="phonenum"]')[0].click()
 
         ele = driver.find_elements_by_xpath('//*[@id="phone"]')[0]
+        #
+        # phonenum = self.yima.generate_phone()
+        # # print('phone:', phonenum)
+        # l.info(f"get phone: {phonenum}")
+        phonenum = 111
 
-        phonenum = self.yima.generate_phone()
-        # print('phone:', phonenum)
-        l.info(f"get phone: {phonenum}")
+        for _ in range(50):
+            phonenum = self.yima.generate_phone()
+            print('phone:', phonenum)
+            if not self.check_is_exists(phonenum):
+                break
+            self.yima.release_num(phonenum)
+
+        if phonenum == 111:
+            l.info(f'get no regist phone error.')
 
         for character in phonenum:
             ele.send_keys(character)
@@ -315,7 +391,6 @@ class DaJie(SpiderBase, Base):
         url = url.replace('https:https://', 'https://')
         retry_time = 15
         time.sleep(6)
-
 
         resumeId = re.findall(r'single/(.*?)\?', url)
         if not resumeId:
