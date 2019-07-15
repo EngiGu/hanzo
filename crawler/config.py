9ec901cc091ef32b323cd38a17c65ab9b04fa5f9
sayhaya <sayhaya@qq.com>
2019-07-15 14:28:46 +0800

    fix 58 hr58

--- crawler/config.py
+++ crawler/config.py
@@ -9,7 +9,7 @@ REDIS_TASK_URI = 'http://192.168.11.191:3333/task'
 # MONGODB_HOST = "mongodb://aizhaopin:aizhaopin%402017@dds-2ze05f9b880291d41.mongodb.rds.aliyuncs.com:3717, dds-2ze05f9b880291d42.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-4570175"
 
 NUM_PER_MACHINE = 20
-NUM_PRE_COOKIES = 10
+NUM_PRE_COOKIES = 2
 
 # rabbitmq
 RABBITMQ_HOST = '192.168.11.191'
