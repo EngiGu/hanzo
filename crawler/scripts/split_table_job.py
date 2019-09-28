404f1ecb387b9e964e140192c6ce509905b22908
sayhaya <sayhaya@qq.com>
2019-09-28 16:51:35 +0800

    添加转换脚本

--- crawler/scripts/split_table_job.py
+++ crawler/scripts/split_table_job.py
@@ -1,5 +1,6 @@
 from core.schema import *
 from core.mysql import Session, engine, session_scope
+from numba import jit
 
 
 def row2dict(row):
@@ -14,7 +15,7 @@ class TD:
 
     def __init__(self):
         self.session = Session()
-        pass
+        self.position_map = {}
 
     def get_old_record(self, offset, limit):
         query = self.session.query(DailyHrCrawl)
@@ -22,9 +23,23 @@ class TD:
             r2d = row2dict(i)
             return r2d
 
+    # @jit
     def extract_position_list(self, result_dict):
-    
-        return
+        position_set = set()
+        new_record = []
+        for r in result_dict:
+            positions = r['positions']
+            if positions:
+                positions = positions.split('、')
+                position_set.update(positions)
+                for i in positions:
+                    new_record.append(
+                        {'jx_resume_id': '3133930034888820', 'position': '厨师/厨师长',
+                         'positions': '厨师/厨师长',
+                         'is_today_update': '1', 'status': '0', 'created': '2019-07-15 22:02:15',
+                         'modified': '2019-07-15 22:02:15'
+                         }
+                    )
 
     def run(self):
         r2d = self.get_old_record(1, 1000)
