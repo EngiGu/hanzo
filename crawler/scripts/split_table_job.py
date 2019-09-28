7add042ff7289fea8fff94114e1cbec099d2e515
sayhaya <sayhaya@qq.com>
2019-09-28 17:25:34 +0800

    update

--- crawler/scripts/split_table_job.py
+++ crawler/scripts/split_table_job.py
@@ -21,29 +21,37 @@ class TD:
     def get_old_record(self, offset, limit):
         return self.session.query(DailyHrCrawl).offset(offset).limit(limit).yield_per(1000)
 
-    # @jit
+    def update_position_tag(self, tag_list):
+        # self.position_set.update(tag_list)
+        for tag in tag_list:
+            if tag not in self.position_map:
+
+        return
+
     def extract_position_list(self, result_query):
         new_record = []
         for r in result_query:
-            print(row2dict(r))
+            # print(row2dict(r))
             positions = r.positions
             if positions:
-                positions = positions.split('、')
-                self.position_set.update(positions)
-                for i in positions:
+                tag_list = positions.split('、')
+                # self.position_set.update(tag_list)
+                self.update_position_tag(tag_list)
+                for i in tag_list:
                     print(i)
                     new_record.append(
                         {
-                            'jx_resume_id': '3133930034888820', 'position': '厨师/厨师长',
-                            'positions': '厨师/厨师长',
-                            'is_today_update': '1', 'status': '0', 'created': '2019-07-15 22:02:15',
-                            'modified': '2019-07-15 22:02:15'
+                            'jx_resume_id': r.jx_resume_id, 'tag_id': '',
+                            'is_excepted': 1 if i == r.position else 0,
+                            'is_today_update': r.is_today_update, 'status': r.status, 'created': r.created,
+                            'modified': r.modified, '__position__': i
                         }
                     )
+        return new_record
 
     def run(self):
         r2d = self.get_old_record(1, 1000)
-        print(r2d)
+        # print(r2d)
         self.extract_position_list(r2d)
         pass
 
