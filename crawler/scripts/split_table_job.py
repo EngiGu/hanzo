a3c57605a18deff50a5d4efe9e85c3201cf7273f
sayhaya <sayhaya@qq.com>
2019-09-28 17:15:49 +0800

    update

--- crawler/scripts/split_table_job.py
+++ crawler/scripts/split_table_job.py
@@ -16,34 +16,35 @@ class TD:
     def __init__(self):
         self.session = Session()
         self.position_map = {}
+        self.position_set = set()
 
     def get_old_record(self, offset, limit):
-        query = self.session.query(DailyHrCrawl)
-        for i in query.offset(offset).limit(limit).yield_per(1000):
-            r2d = row2dict(i)
-            return r2d
+        return self.session.query(DailyHrCrawl).offset(offset).limit(limit).yield_per(1000)
 
     # @jit
-    def extract_position_list(self, result_dict):
-        position_set = set()
+    def extract_position_list(self, result_query):
         new_record = []
-        for r in result_dict:
-            positions = r['positions']
+        for r in result_query:
+            print(row2dict(r))
+            positions = r.positions
             if positions:
                 positions = positions.split('、')
-                position_set.update(positions)
+                self.position_set.update(positions)
                 for i in positions:
+                    print(i)
                     new_record.append(
-                        {'jx_resume_id': '3133930034888820', 'position': '厨师/厨师长',
-                         'positions': '厨师/厨师长',
-                         'is_today_update': '1', 'status': '0', 'created': '2019-07-15 22:02:15',
-                         'modified': '2019-07-15 22:02:15'
-                         }
+                        {
+                            'jx_resume_id': '3133930034888820', 'position': '厨师/厨师长',
+                            'positions': '厨师/厨师长',
+                            'is_today_update': '1', 'status': '0', 'created': '2019-07-15 22:02:15',
+                            'modified': '2019-07-15 22:02:15'
+                        }
                     )
 
     def run(self):
         r2d = self.get_old_record(1, 1000)
-
+        print(r2d)
+        self.extract_position_list(r2d)
         pass
 
 
