a7d8a97e27f77ee6f5211abd5fd825fa9003beb8
sayhaya <sayhaya@qq.com>
2019-09-28 18:28:45 +0800

    update

--- crawler/scripts/split_table_job.py
+++ crawler/scripts/split_table_job.py
@@ -18,8 +18,12 @@ class TD:
         self.position_map = {}
         self.position_set = set()
 
-    def get_old_record(self, offset, limit):
-        return self.session.query(DailyHrCrawl).offset(offset).limit(limit).yield_per(1000)
+    def get_old_record(self, day):
+        # return self.session.query(DailyHrCrawl).offset(offset).limit(limit).yield_per(1000)
+        return self.session.query(DailyHrCrawl).filter(
+            DailyHrCrawl.created >= '{} 00:00:00'.format(day),
+            DailyHrCrawl.created <= '{} 23:59:59'.format(day),
+        ).yield_per(1000)
 
     def get_position_tag_id(self, tag):
         query = self.session.query(PositionTag.id).filter(PositionTag.position == tag).first()
@@ -30,9 +34,14 @@ class TD:
             return position_tag.id
         return query.id
 
+    @jit
     def save_crawled_record(self, r_list):
-        self.session.bulk_insert_mappings(DailyHrCrawlNew, r_list)
-        self.session.commit()
+        i = 0
+        step = 2000
+        for i in range(0, len(r_list), step):
+            print('saving',i, i + step)
+            self.session.bulk_insert_mappings(DailyHrCrawlNew, r_list[i:i + step])
+            self.session.commit()
 
     @jit
     def update_position_tag(self, tag_list):
@@ -53,7 +62,7 @@ class TD:
                 # self.position_set.update(tag_list)
                 self.update_position_tag(tag_list)
                 for i in tag_list:
-                    print(i)
+                    # print(i)
                     new_record.append(
                         {
                             'jx_resume_id': r.jx_resume_id, 'tag_id': '',
@@ -75,20 +84,60 @@ class TD:
 
     def run(self):
         # 获取旧的简历
-        r2d = self.get_old_record(1, 1000)
-        # print(r2d)
-        # 拆分处理
-        r_list = self.extract_position_list(r2d)
-        r_list = self.rebuild_list(r_list)
+        days = ['2019-09-27']
+        for day in days:
+            r2d = self.get_old_record(day)
+            # print(r2d)
+            # 拆分处理
+            r_list = self.extract_position_list(r2d)
+            r_list = self.rebuild_list(r_list)
 
-        # print(r_list)
-        print(self.position_map)
-        # 插入新的数据表
-        self.save_crawled_record(r_list)
-
-        pass
+            # print(r_list)
+            # print(self.position_map)
+            # 插入新的数据表
+            self.save_crawled_record(r_list)
 
 
 if __name__ == '__main__':
+    # all day
+    """
+    2019-07-15
+2019-07-16
+2019-07-17
+2019-07-18
+2019-07-19
+2019-07-20
+2019-07-21
+2019-08-03
+2019-08-04
+2019-08-05
+2019-08-06
+2019-08-09
+2019-08-14
+2019-08-15
+2019-08-16
+2019-08-17
+2019-08-18
+2019-08-19
+2019-08-20
+2019-08-21
+2019-08-22
+2019-08-23
+2019-08-24
+2019-08-25
+2019-08-26
+2019-08-27
+2019-08-28
+2019-08-29
+2019-09-19
+2019-09-20
+2019-09-21
+2019-09-22
+2019-09-23
+2019-09-24
+2019-09-25
+2019-09-26
+2019-09-27
+    """
     t = TD()
     t.run()
